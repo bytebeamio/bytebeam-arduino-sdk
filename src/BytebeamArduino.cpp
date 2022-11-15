6ec5906b4d27d5545de05efad764a789789f3482
mayankbytebeam <mayanknarang@bytebeam.io>
2022-11-15 15:16:01 +0530

    error checking and global object added

--- src/BytebeamArduino.cpp
+++ src/BytebeamArduino.cpp
@@ -1,31 +1,41 @@
 #include "BytebeamArduino.h"
 
-Bytebeam::Bytebeam() {
-  Serial.println("I am Bytebeam::Bytebeam()");
+void subscribeCallback(char* topic, byte* message, unsigned int length) {
+  Serial.println("I am subscribeCallback()");
+
+  Serial.print("{Topic : ");
+  Serial.print(topic);
+  Serial.print(", Message : ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)message[i]);
+  }
+  Serial.println("}");
 }
 
-Bytebeam::~Bytebeam() {
-  Serial.println("I am Bytebeam::~Bytebeam()");
+BytebeamArduino::BytebeamArduino() {
+  Serial.println("I am BytebeamArduino::BytebeamArduino()");
+}
+
+BytebeamArduino::~BytebeamArduino() {
+  Serial.println("I am BytebeamArduino::~BytebeamArduino()");
 } 
 
-void Bytebeam::readDeviceConfigFile() {
-  Serial.println("I am Bytebeam::readDeviceConfigFile()");
-
+bool BytebeamArduino::readDeviceConfigFile() {
   if (!SPIFFS.begin(true, "/spiffs")) {
     Serial.println("spiffs mount failed");
-    return;
+    return false;
   } else {
     Serial.println("spiffs mount success");
   }
 
   const char * path = "/device_config.json";
-  Serial.printf("Reading file : %s\r\n", path);
+  Serial.printf("Reading file : %s\n", path);
 
   fs::FS &fs = SPIFFS;
   File file = fs.open(path, FILE_READ);
   if (!file || file.isDirectory()) {
     Serial.println("- failed to open file for reading");
-    return;
+    return false;
   }
 
   char chr = ' ';
@@ -36,19 +46,29 @@ void Bytebeam::readDeviceConfigFile() {
     chr = file.read();
     this->deviceConfigStr[strIndex++] = chr;
   }
+
   file.close();
-
-  // Serial.println("deviceConfigStr :");
-  // Serial.println(this->deviceConfigStr);
-
   SPIFFS.end();
+
+  #if DEBUG_BYTEBEAM_ARDUINO
+    Serial.println("deviceConfigStr :");
+    Serial.println(this->deviceConfigStr);
+  #endif
+
+  return true;
 }
 
-void Bytebeam::parseDeviceConfigFile() {
-  Serial.println("I am Bytebeam::parseDeviceConfigFile()");
-
-  deserializeJson(this->deviceConfigJson, this->deviceConfigStr);
+boolean BytebeamArduino::parseDeviceConfigFile() {
+  DeserializationError err = deserializeJson(this->deviceConfigJson, this->deviceConfigStr);
+  if(err) {
+    Serial.printf("deserializeJson() failed : %s\n", err.c_str());
+    return false;
+  } else {
+    Serial.println("deserializeJson() success");
+  }
   
+  Serial.println("Obtaining device variables");
+
   this->mqttPort      = this->deviceConfigJson["port"];
   this->mqttBrokerUrl = this->deviceConfigJson["broker"];
   this->deviceId      = this->deviceConfigJson["device_id"];
@@ -57,68 +77,129 @@ void Bytebeam::parseDeviceConfigFile() {
   this->clientCertPem = this->deviceConfigJson["authentication"]["device_certificate"];
   this->clientKeyPem  = this->deviceConfigJson["authentication"]["device_private_key"];
   
-  // Serial.println(this->mqttPort);
-  // Serial.println(this->mqttBrokerUrl);
-  // Serial.println(this->deviceId);
-  // Serial.println(this->projectId);
-  // Serial.println(this->caCertPem);
-  // Serial.println(this->clientCertPem);
-  // Serial.println(this->clientKeyPem);
+  const char* name[] = {"broker", "device_id", "project_id", "ca_certificate", "device_certificate", "device_private_key"};
+  const char* args[] = {this->mqttBrokerUrl, this->deviceId, this->projectId, this->caCertPem, this->clientCertPem, this->clientKeyPem};
+  int numArg = sizeof(args)/sizeof(args[0]);
+  
+  int argIterator = 0;
+  for(argIterator = 0; argIterator < numArg; argIterator++) {
+    if(args[argIterator] == NULL) {
+      Serial.printf("- failed to obtain %s\n", name[argIterator]);
+      return false;
+    }
+  }
+  Serial.println("- obtain device variables");
+
+  #if DEBUG_BYTEBEAM_ARDUINO
+    Serial.println(this->mqttPort);
+    Serial.println(this->mqttBrokerUrl);
+    Serial.println(this->deviceId);
+    Serial.println(this->projectId);
+    Serial.println(this->caCertPem);
+    Serial.println(this->clientCertPem);
+    Serial.println(this->clientKeyPem);
+  #endif
 
   secureClient.setCACert(this->caCertPem);
   secureClient.setCertificate(this->clientCertPem); 
   secureClient.setPrivateKey(this->clientKeyPem);
+
+  return true;
 }
 
-boolean Bytebeam::begin() {
-  Serial.println("I am Bytebeam::begin()");
-
-  readDeviceConfigFile();
-  parseDeviceConfigFile();
+boolean BytebeamArduino::begin() {
+  if(!readDeviceConfigFile()) {
+    Serial.println("begin abort, error while reading the device config file...");
+    return false;
+  }
+            
+  if(!parseDeviceConfigFile()) {
+    Serial.println("begin abort, error while parsing the device config file...");
+    return false;
+  }
 
   PubSubClient::setClient(secureClient);
+  PubSubClient::setCallback(subscribeCallback);
   PubSubClient::setServer(mqttBrokerUrl, mqttPort);
-  return PubSubClient::connect("BytebeamArduino");
+
+  if(!PubSubClient::connect("BytebeamArduino")) {
+    Serial.println("error while connecting to server...");
+    return false;
+  } else {
+    Serial.println("bytebeam client connected successfully !");
+    return true;
+  }
 }
 
-void Bytebeam::setSubscribeCallback(MQTT_CALLBACK_SIGNATURE) {
-  Serial.println("I am Bytebeam::setSubscribeCallback()");
+boolean BytebeamArduino::publish(const char* topic, const char* payload) {
+  if(!PubSubClient::connected()) {
+    Serial.printf("publish abort, topic : %s. bytebeam client is not connected to the server...\n", topic);
+    return false;
+  }
 
-  PubSubClient::setCallback(callback);
+  if(!PubSubClient::publish(topic, payload)) {
+    Serial.printf("publish failed, topic : %s\n", topic);
+    return false;
+  } else {
+    Serial.printf("publish success, topic : %s\n", topic);
+    return true;
+  }
 }
 
-boolean Bytebeam::publish(const char* topic, const char* payload) {
-  Serial.println("I am Bytebeam::publish()");
+boolean BytebeamArduino::subscribe(const char* topic) {
+  if(!PubSubClient::connected()) {
+    Serial.printf("subscribe abort, topic : %s. bytebeam client is not connected to the server...\n", topic);
+    return false;
+  }
 
-  return PubSubClient::publish(topic, payload);
+  if(!PubSubClient::subscribe(topic)) {
+    Serial.printf("subscribe failed, topic : %s\n", topic);
+    return false;
+  } else {
+    Serial.printf("subscribe success, topic : %s\n", topic);
+    return true;
+  }
 }
 
-boolean Bytebeam::subscribe(const char* topic) {
-  Serial.println("I am Bytebeam::subscribe()");
+boolean BytebeamArduino::unsubscribe(const char* topic) {
+  if(!PubSubClient::connected()) {
+    Serial.printf("unsubscribe abort, topic : %s. bytebeam client is not connected to the server...\n", topic);
+    return false;
+  }
 
-  return  PubSubClient::subscribe(topic);
+  if(!PubSubClient::unsubscribe(topic)) {
+    Serial.printf("unsubscribe failed, topic : %s\n", topic);
+    return false;
+  } else {
+    Serial.printf("unsubscribe success, topic : %s\n", topic);
+    return true;
+  }
 }
 
-boolean Bytebeam::unsubscribe(const char* topic) {
-  Serial.println("I am Bytebeam::unsubscribe()");
+boolean BytebeamArduino::loop() {
+  Serial.println("I am BytebeamArduino::loop()");
 
-  return  PubSubClient::unsubscribe(topic);
+  if(!PubSubClient::loop()) {
+    Serial.println("error while maintaining the connection to the server...");
+    return false;
+  }
+
+  return true;
 }
 
-boolean Bytebeam::loop() {
-  Serial.println("I am Bytebeam::loop()");
-
-  return  PubSubClient::loop();
+boolean BytebeamArduino::connected() {
+  if(!PubSubClient::connected()) {
+    Serial.println("bytebeam client is not connected to the server !");
+    return false;
+  } else {
+    Serial.println("bytebeam client is connected to the server !");
+    return true;
+  }
 }
 
-boolean Bytebeam::connected() {
-  Serial.println("I am Bytebeam::connected()");
-
-  return  PubSubClient::connected();
-}
-
-void Bytebeam::end() {
-  Serial.println("I am Bytebeam::end()");
-
+void BytebeamArduino::end() {
   PubSubClient::disconnect();
+  Serial.println("bytebeam client disconnected successfully !");
 }
+
+BytebeamArduino Bytebeam;
--- src/BytebeamArduino.h
+++ src/BytebeamArduino.h
@@ -10,36 +10,41 @@
 #include <PubSubClient.h>
 #include <WiFiClientSecure.h>
 
+/* This macro is used to debug the library, we will keep all the unnecessary print under this macro */
+#define DEBUG_BYTEBEAM_ARDUINO false
 
-#define DEVICE_CONFIG_STR_LENGTH 8192  // maximum size of json object in bytes
+/* This macro is used to specify the maximum size of device config json in bytes that need to be handled for particular device */
+#define DEVICE_CONFIG_STR_LENGTH 8192
 
-class Bytebeam : private PubSubClient {
+class BytebeamArduino : private PubSubClient {
 public:
     // contructor
-    Bytebeam();
+    BytebeamArduino();
 
     // destructor
-    ~Bytebeam();
+    ~BytebeamArduino();
 
     // public functions
     boolean begin();
-    void setSubscribeCallback(MQTT_CALLBACK_SIGNATURE);
-    boolean publish(const char* topic, const char* payload);
-    boolean subscribe(const char* topic);
-    boolean unsubscribe(const char* topic);
     boolean loop();
     boolean connected();
+
+    /* pub sub api's */
+    boolean subscribe(const char* topic);
+    boolean unsubscribe(const char* topic);
+    boolean publish(const char* topic, const char* payload);
+
     void end();
 
 private:
     // private functions 
-    void readDeviceConfigFile();
-    void parseDeviceConfigFile();
+    boolean readDeviceConfigFile();
+    boolean parseDeviceConfigFile();
 
     // private variables
     char deviceConfigStr[DEVICE_CONFIG_STR_LENGTH];
     StaticJsonDocument<DEVICE_CONFIG_STR_LENGTH> deviceConfigJson;
-
+    
     uint16_t mqttPort;
     const char* mqttBrokerUrl; 
     const char* deviceId;
@@ -51,5 +56,7 @@ private:
   
     WiFiClientSecure secureClient;
 };
+
+extern BytebeamArduino Bytebeam;
 
 #endif /* BYTEBEAM_ARDUINO_H */ 
