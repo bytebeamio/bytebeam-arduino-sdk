d080748bcc04f3da049ea308954ded554b321bd4
mayankbytebeam <mayanknarang@bytebeam.io>
2023-02-17 20:27:02 +0530

    esp touch example added

---
+++ examples/EspTouch/EspTouch.ino
@@ -0,0 +1,151 @@
+#include <time.h>
+#include <WiFi.h>
+#include <BytebeamArduino.h>
+
+// touch gpio pin number
+#define TOUCH_GPIO_PIN 15
+
+// esp touch stream name
+char espTouchStream[] = "esp_touch";
+
+// wifi credentials
+const char* WIFI_SSID     = "YOUR_WIFI_SSID";
+const char* WIFI_PASSWORD = "YOUR_WIFI_PASSWORD";
+
+// sntp credentials
+const long  gmtOffset_sec = 19800;
+const int   daylightOffset_sec = 3600;
+const char* ntpServer = "pool.ntp.org";
+
+// function to setup the wifi with predefined credentials
+void setupWifi() {
+  // set the wifi to station mode to connect to a access point
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID , WIFI_PASSWORD);
+
+  Serial.println();
+  Serial.print("Connecting to " + String(WIFI_SSID));
+
+  // wait till chip is being connected to wifi  (Blocking Mode)
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(250);
+  }
+
+  // now it is connected to the access point just print the ip assigned to chip
+  Serial.println();
+  Serial.print("Connected to " + String(WIFI_SSID) + ", Got IP address : ");
+  Serial.println(WiFi.localIP());
+}
+
+// function to sync time from ntp server with predefined credentials
+void syncTimeFromNtp() {
+  // sync the time from ntp server
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+
+  struct tm timeinfo;
+
+  // get the current time
+  if(!getLocalTime(&timeinfo)) {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+
+  // log the time info to serial :)
+  Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+  Serial.println();
+}
+
+// function to get the esp touch value
+uint16_t getEspTouchValue() {
+  return touchRead(TOUCH_GPIO_PIN);
+}
+
+// function to get the time 
+unsigned long long getEpochMillis() {
+  time_t now;
+  struct tm timeinfo;
+
+  // get the current time i.e make sure the device is in sync with the ntp server
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("failed to obtain time");
+    return 0;
+  }
+
+  // get the epoch time
+  time(&now);
+
+  // generate the epoch millis
+  unsigned long long timeMillis = ((unsigned long long)now * 1000) + (millis() % 1000);
+
+  return timeMillis;
+}
+
+// function to publish esp touch value to esp touch stream
+boolean publishEspTouchValues() {
+  static int sequence = 0;
+  unsigned long long milliseconds = 0;
+
+  const char* payload = "";
+  String deviceShadowStr = "";
+  StaticJsonDocument<1024> doc;
+
+  // get the current epoch millis
+  milliseconds = getEpochMillis();
+
+  // make sure you got the millis
+  if(milliseconds == 0) {
+    Serial.println("failed to get epoch millis");
+    return false;
+  }
+
+  // increment the sequence counter
+  sequence++;
+
+  // get the connection status
+  bool connectionStatus = Bytebeam.isConnected();
+
+  // get the esp touch value
+  uint16_t touchValue = getEspTouchValue();
+
+  JsonArray deviceShadowJsonArray = doc.to<JsonArray>();
+  JsonObject deviceShadowJsonObj_1 = deviceShadowJsonArray.createNestedObject();
+
+  deviceShadowJsonObj_1["timestamp"] = milliseconds;
+  deviceShadowJsonObj_1["sequence"]  = sequence;
+  deviceShadowJsonObj_1["touch"]     = touchValue;
+  
+  serializeJson(deviceShadowJsonArray, deviceShadowStr);
+  payload = deviceShadowStr.c_str();
+
+  Serial.printf("publishing %s to %s\n", payload, espTouchStream);
+
+  return Bytebeam.publishToStream(espTouchStream, payload);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println();
+
+  setupWifi();
+  syncTimeFromNtp();
+  
+  // begin the bytebeam client
+  Bytebeam.begin();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  // bytebeam client loop
+  Bytebeam.loop();
+
+  // publish device status to device shadow
+  if(!publishEspTouchValues()) {
+    Serial.println("Failed to publish esp touch value to esp touch stream");
+  }
+
+  // hold on execution for some time
+  delay(10000);
+}
---
+++ examples/EspTouch/README.md
@@ -0,0 +1,8 @@
+# Bytebeam Esp Touch Example
+ESP32 comes with capacitive touch GPIOs which can sense human touch. We will make use of these touch GPIO to send touch values to bytebeam cloud
+
+## Hardware specification
+1. ESP32 Dev Board
+
+## Software Specification
+1. Bytebeam Arduino Library
